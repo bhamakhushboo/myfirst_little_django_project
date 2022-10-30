02f09f3c7ae9cb9e6c44a3a4fe5a545ef61fcbaf
bhamakhushboo <bahadoor.b@yahoo.com>
2022-10-30 19:25:57 +0400

    updated before moving to dissert

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
+
 def index(request):
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = "Fast"
+    feature1.details = "Our service is very quick"
+
     context = {
-        'name': 'Patrick',
-        'age': 23,
-        'nationality': 'British'
+      'name': 'Patrick',
+      'age': 23,
+      'nationality': 'British'
+      }
+    return render(request, 'index.html', {'feature': feature1})
+
+def counter(request):
+    words = request.POST['words']
+    amt_of_word = len(words.split())
+    context = {
+        'amt': amt_of_word,
+        'words': words
     }
-    return render(request, 'index.html', context)
+    return render(request, 'counter.html', context)
     
